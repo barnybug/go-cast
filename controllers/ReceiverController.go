ca656d222be29b7fbe6e366ed0ca00b952b573e9
Elliot Shepherd <elliot@ninjablocks.com>
2014-10-22 13:47:33 +1100

    Expose ReceiverController, add SetVolume, GetVolume

--- controllers/ReceiverController.go
+++ controllers/ReceiverController.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"encoding/json"
 	"time"
 
 	"github.com/davecgh/go-spew/spew"
@@ -8,15 +9,15 @@ import (
 	"github.com/ninjasphere/go-cast/api"
 )
 
-type receiverController struct {
+type ReceiverController struct {
 	interval time.Duration
 	channel  *cast.Channel
 }
 
 var getStatus = cast.PayloadHeaders{Type: "GET_STATUS"}
 
-func NewReceiverController(client *cast.Client, sourceId, destinationId string) *receiverController {
-	controller := &receiverController{
+func NewReceiverController(client *cast.Client, sourceId, destinationId string) *ReceiverController {
+	controller := &ReceiverController{
 		channel: client.NewChannel(sourceId, destinationId, "urn:x-cast:com.google.cast.receiver"),
 	}
 
@@ -25,10 +26,35 @@ func NewReceiverController(client *cast.Client, sourceId, destinationId string) 
 	return controller
 }
 
-func (c *receiverController) onStatus(message *api.CastMessage) {
+func (c *ReceiverController) onStatus(message *api.CastMessage) {
 	spew.Dump("Got status message", message)
 }
 
-func (c *receiverController) GetStatus(timeout time.Duration) (*api.CastMessage, error) {
+type VolumePayload struct {
+	cast.PayloadHeaders
+	Volume *float64 `json:"volume,omitempty"`
+	Mute   *bool    `json:"mute,omitempty"`
+}
+
+func (c *ReceiverController) GetStatus(timeout time.Duration) (*api.CastMessage, error) {
 	return c.channel.Request(&getStatus, timeout)
+}
+
+func (c *ReceiverController) SetVolume(volume *VolumePayload, timeout time.Duration) (*api.CastMessage, error) {
+	volume.Type = "SET_VOLUME"
+	return c.channel.Request(volume, timeout)
+}
+
+func (c *ReceiverController) GetVolume(timeout time.Duration) (*VolumePayload, error) {
+	message, err := c.GetStatus(timeout)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var volume VolumePayload
+
+	err = json.Unmarshal([]byte(*message.PayloadUtf8), &volume)
+
+	return &volume, err
 }
