0624ab4039d0b60afa48f868e995d5b432d1bc53
☃ Elliot Shepherd <elliot@ninjablocks.com>
2015-11-23 13:39:34 +1100

    Note to self: Actually check PRs :)

--- api/cast_channel.pb.go
+++ api/cast_channel.pb.go
@@ -17,7 +17,7 @@ It has these top-level messages:
 */
 package api
 
-import proto "github.com/gogo/protobuf"
+import proto "github.com/gogo/protobuf/proto"
 import json "encoding/json"
 import math "math"
 
